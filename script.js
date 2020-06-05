05a68722b90ecbfe710162430bdbbae9d9a87128
ugurduyar <ismailugurduyar@gmail.com>
2020-06-05 20:56:48 +0300

    push data to obj

--- script.js
+++ script.js
@@ -16,5 +16,15 @@ async function getRandomUser() {
   const res = await fetch("https://randomuser.me/api");
   const data = await res.json();
 
-  console.log(data);
+  const user = data.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+  addData(newUser);
+}
+
+// Add new obj to data arr
+function addData(obj) {
+  data.push(obj);
 }
