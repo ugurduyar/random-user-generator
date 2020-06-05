904dc0c773ac60de2bd16bebbea8c7b57d8cb545
ugurduyar <ismailugurduyar@gmail.com>
2020-06-05 21:08:29 +0300

    Get random user

--- script.js
+++ script.js
@@ -50,3 +50,6 @@ function updateDOM(providedData = data) {
 function formatMoney(number) {
   return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
 }
+
+// Event listeners
+addUserBtn.addEventListener("click", getRandomUser);
