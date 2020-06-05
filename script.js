1946e495de6d5a751b840d06ff78934fb34661b9
ugurduyar <ismailugurduyar@gmail.com>
2020-06-05 21:56:36 +0300

    show only millionaires done

--- script.js
+++ script.js
@@ -39,6 +39,12 @@ function sortByRichest() {
   updateDOM();
 }
 
+// Filter millionaires only
+function showMillionaires() {
+  data = data.filter((user) => user.money > 1000000);
+  updateDOM();
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
@@ -70,3 +76,4 @@ function formatMoney(number) {
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
