d9fad8609fb4885eac5415ec382f6739520e7bc1
ugurduyar <ismailugurduyar@gmail.com>
2020-06-05 22:01:21 +0300

    calculate entire wealth

--- script.js
+++ script.js
@@ -45,6 +45,17 @@ function showMillionaires() {
   updateDOM();
 }
 
+// Calculate total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}  </strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
@@ -77,3 +88,4 @@ addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
 showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
